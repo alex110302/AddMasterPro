08d0dfe8b54b1d741de9f7e4cd8f0e33d41ccf22
alex110302 <hansenaalexander@gmail.com>
2024-09-13 12:36:06 -0700

    Made the add master pro

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace AddMaster;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        Title = "add Master Pro";
+    }
+
+    private void Add_OnClicked(object sender, EventArgs e)
+    {
+        double dblFirst, dblSecond;
+        double.TryParse(txtFirstNumber.Text, out dblFirst);
+        double.TryParse(txtSecondNumber.Text, out dblSecond);
+
+        lblDisplay.Text = (dblFirst + dblSecond).ToString();
+    }
+    
+    private void Clear_OnClicked(object sender, EventArgs e)
+    {
+        txtFirstNumber.Text = string.Empty;
+        txtSecondNumber.Text = string.Empty;
+        lblDisplay.Text = "0";
+    }
+
+}
